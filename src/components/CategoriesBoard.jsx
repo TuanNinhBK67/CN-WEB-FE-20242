82fc6e2a7c84c2cc35ebf5aad529f4ddd10f2908
TuanNinhBK67 <tuanninhhust2022@gmail.com>
2025-05-12 13:16:03 +0700

    update homepage

---
+++ src/components/CategoriesBoard.jsx
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+import "../assets/scss/categoriesboard.scss";
+import {getAllCategories} from "../services/productService"
+
+const SidebarCategories = () => {
+    const [categories, setCategories] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getAllCategories()
+        .then((res) => setCategories(res.data))
+        .catch((err) => console.error("Lỗi khi lấy danh mục:", err));
+    }, []);
+
+    const handleCategoryClick = (categoryId) => {
+        // Sửa link đoạn này cho tôi với 
+        navigate(`/?category_id=${categoryId}`);
+    };
+
+    return (
+        <div className="sidebar-categories">
+        <h3>Danh mục sản phẩm</h3>
+        <ul>
+            {categories.map((cat) => (
+            <li key={cat.id}>
+                <button onClick={() => handleCategoryClick(cat.id)}>
+                {cat.name}
+                </button>
+            </li>
+            ))}
+        </ul>
+        </div>
+    );
+}
+
+export default SidebarCategories;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,63 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer"
+import { getAllProducts } from "../services/productService";
+import "../assets/scss/homepage.scss"
+import SidebarCategories from "../components/CategoriesBoard";
+import { Link } from "react-router-dom";
 
-const homepage = () => {
-    return(
+const HomePage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getAllProducts()
+        .then((res) => setProducts(res.data))
+        .catch((err) => console.error("Lỗi khi lấy sản phẩm:", err));
+    }, []);
+
+    return (
         <>
-            <Header></Header>
-            <h1>This is homepage</h1>
-            <Footer></Footer>
+        <Header></Header>
+        <div className="home-layout">
+
+        <aside className="sidebar">
+            <SidebarCategories></SidebarCategories>
+        </aside>
+
+        <div className="home-container">
+        {/* <h2 className="home-title">Danh sách sản phẩm</h2> */}
+        <div className="product-grid">
+            {products.map((p) => (
+                // sửa cái link thành chuyển sang sản phẩm cho tôi nhé 
+            <Link to={'/'} className="product-card" key={p.id}>
+                <img src={p.image_url} alt={p.name} className="product-img" />
+                <h3 className="product-name">{p.name}</h3>
+                <div className="product-price">
+                    {Number(p.discount) > 0 ? (
+                        <>
+                             <span className="price-original">
+                                {p.price.toLocaleString()}đ
+                            </span>
+                            <span className="price-discounted">
+                                {(p.price - p.discount).toLocaleString()}đ
+                            </span>
+                        </>
+                    ) : (
+                        <>
+                            <span className="price-discounted">
+                                {p.price.toLocaleString()}đ
+                            </span>
+                        </>
+                    )}
+                </div>
+            </Link>
+            ))}
+        </div>
+        </div>
+        </div>
+        <Footer></Footer>
         </>
-    )
+    );
+
 }
-export default homepage;
+export default HomePage;
